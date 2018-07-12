1472dca932bdfcc927c76834f55c8f93440c37a6
Jean Jourdain <jean.jourdain@mixio.com>
2018-07-12 16:31:20 +0200

    Truncated the file paths from beginning to "Source"

--- Sources/JJTools/JJTools.swift
+++ Sources/JJTools/JJTools.swift
@@ -2,12 +2,29 @@ import Foundation
 
 public func jjprint(_ items: Any..., file: String = #file, line: Int = #line) {
 
-    let header = """
-        ---------------
-        \(file):\(line)
-        ---------------
-        """
-   print(header)
+    if let path = jjTruncatedPath(file) {
+        let header = """
+            ---------------
+            \(path):\(line)
+            ---------------
+            """
+       print(header)
+    }
    print(items)
+
+}
+
+private func jjTruncatedPath(_ path: String) -> String? {
+
+    guard let url = URL(string: path) else {
+        return nil
+    }
+
+    var pathComponents = url.pathComponents
+    while pathComponents.first != "Source" {
+        pathComponents.removeFirst()
+    }
+    let truncatedPath = pathComponents.joined(separator: "/")
+    return truncatedPath
 
 }
