aa2e1de96b6c725f39c3ae8860fa075bc4392517
Jean Jourdain <jean.jourdain@mixio.com>
2018-07-13 11:52:48 +0200

    Added jjprintex()

--- Sources/JJTools/JJTools.swift
+++ Sources/JJTools/JJTools.swift
@@ -12,6 +12,24 @@ public func jjprint(_ items: Any..., file: String = #file, line: Int = #line) {
     print(items)
 }
 
+public func jjprintex(_ items: Any..., file: String = #file, line: Int = #line) {
+    if let path = jjTruncatedPathToSources(file) {
+        let header = """
+        +-----------------------------
+        | 📄 | \(path):\(line)
+        +-----------------------------
+        """
+        print(header)
+    }
+    print(items)
+
+    let stackTrace = Thread.callStackSymbols
+    print("+-----------------------------")
+    print(stackTrace)
+    print("+-----------------------------")
+
+}
+
 private func jjTruncatedPathToSources(_ path: String) -> String? {
     var pathComponents = path.split(separator: "/")
     if let indexOfSources = pathComponents.firstIndex(of: "Sources") {
