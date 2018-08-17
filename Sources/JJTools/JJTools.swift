4130ab536b6b36f8b82d608765b8399e8388c538
Jean Jourdain <jean.jourdain@mixio.com>
2018-08-17 14:07:08 +0200

    Changed firstIndex(of:) to firstIndex(where:) because the firstIndex(of:) is only available in macos(10.3) and swift 4.1.2.

--- Sources/JJTools/JJTools.swift
+++ Sources/JJTools/JJTools.swift
@@ -32,7 +32,13 @@ public func jjprintex(_ items: Any..., file: String = #file, line: Int = #line) 
 
 private func jjTruncatedPathToSources(_ path: String) -> String? {
     var pathComponents = path.split(separator: "/")
-    if let indexOfSources = pathComponents.firstIndex(of: "Sources") {
+    let indexOfSources: Array<[String.SubSequence]>.Index?
+    if #available(macOS 10.3, *) {
+        indexOfSources = pathComponents.firstIndex(of: "Sources")
+    } else {
+        indexOfSources = pathComponents.firstIndex(where: { $0 == "Sources" })
+    }
+    if let indexOfSources = indexOfSources {
         pathComponents.removeFirst(indexOfSources + 1)
         let truncatedPath = pathComponents.joined(separator: "/")
         return truncatedPath
